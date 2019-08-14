868b9f29ba1b0ab74521dd1c9aa782ae9f9d2680
miguelfreelancer56577 <miguelfreelancer56577@gmail.com>
2019-08-14 10:14:15 -0500

    Solved issue to get the document client.

--- src/main/java/com/github/mangelt/sat/services/config/CosmosConfiguration.java
+++ src/main/java/com/github/mangelt/sat/services/config/CosmosConfiguration.java
@@ -1,15 +1,16 @@
 package com.github.mangelt.sat.services.config;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Repository;
 
-import com.azure.data.cosmos.ConnectionPolicy;
-import com.azure.data.cosmos.ConsistencyLevel;
-import com.azure.data.cosmos.internal.AsyncDocumentClient;
-import com.github.mangelt.sat.services.util.ConfigConstant;
 
-import lombok.extern.slf4j.Slf4j;
+import com.github.mangelt.sat.services.util.ConfigConstant;
+import com.microsoft.azure.cosmosdb.rx.AsyncDocumentClient;
+import com.microsoft.azure.cosmosdb.ConnectionPolicy;
+import com.microsoft.azure.cosmosdb.ConsistencyLevel;
 
 @Slf4j
 @Repository
@@ -31,12 +32,21 @@ public class CosmosConfiguration{
     	if(ConfigConstant.defultProperty.equals(this.accountHost) || ConfigConstant.defultProperty.equals(this.accountKey)){
     		return null;
     	}
+    	
+    	ConnectionPolicy connPolicy = new ConnectionPolicy();
+        connPolicy.setConnectionMode(com.microsoft.azure.cosmosdb.ConnectionMode.Gateway);
+        connPolicy.setMaxPoolSize(50);
+        com.microsoft.azure.cosmosdb.RetryOptions retryOptions = new com.microsoft.azure.cosmosdb.RetryOptions();
+        retryOptions.setMaxRetryAttemptsOnThrottledRequests(3);
+        retryOptions.setMaxRetryWaitTimeInSeconds(1);
+        connPolicy.setRetryOptions(retryOptions);
+    	
     	return new AsyncDocumentClient
         		.Builder()
         		.withServiceEndpoint(accountHost)
         		.withMasterKeyOrResourceToken(accountKey)
-        		.withConnectionPolicy(ConnectionPolicy.defaultPolicy())
-        		.withConsistencyLevel(ConsistencyLevel.EVENTUAL)
+        		.withConnectionPolicy(connPolicy)
+        		.withConsistencyLevel(ConsistencyLevel.Session)
         		.build();
     }
 
